c0677315a07a525bfcec571c86a4b01ba1656f70
mintmaxminto <hassanw728@gmail.com>
2022-03-14 12:22:53 +0500

    refactoring anf bug fixing

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import parse from 'html-react-parser';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
+import parse from 'html-react-parser';
 
 import { getComments } from '../services';
 
@@ -8,36 +8,36 @@ const Comments = ({ slug }) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    getComments(slug)
-      .then((result) =>  setComments(result))
-  }, [])
-  
+    getComments(slug).then((result) => {
+      setComments(result);
+    });
+  }, []);
 
   return (
     <>
       {comments.length > 0 && (
-        <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
-          <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
             {comments.length}
             {' '}
             Comments
           </h3>
-          {comments.map((comment) => (
-            <div key={comment.createdAt} className='border-b border-gray-100 mb-4 pb-4'>
-              <p className='mb-4'>
-                <span className='font-semibold'>{comment.name}</span>
-                {' '}
-                on
-                {' '}
-                {moment(comment.createdAt).format('MMM DD, YYYY')}
-              </p>
-              <p className='whitespace-pre-line text-gray-600 w-full'>{parse(comment.comment)}</p>
-            </div>
-          ))}
+            {comments.map((comment, index) => (
+              <div key={index} className="border-b border-gray-100 mb-4 pb-4">
+                <p className="mb-4">
+                  <span className="font-semibold">{comment.name}</span>
+                  {' '}
+                  on
+                  {' '}
+                  {moment(comment.createdAt).format('MMM DD, YYYY')}
+                </p>
+                <p className="whitespace-pre-line text-gray-600 w-full">{parse(comment.comment)}</p>
+              </div>
+            ))}
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
