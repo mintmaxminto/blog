55fe999b47ff96f9cc8a512edff9619dbeadf069
mintmaxminto <hassanw728@gmail.com>
2022-03-14 12:32:43 +0500

    fixed author related component

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+import { AdjacentPosts } from '../../sections';
 import { getPosts, getPostDetails } from '../../services';
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 
@@ -17,6 +18,7 @@ const PostDetails = ({ post }) => {
                 <div className='col-span-1 lg:col-span-8'>
                     <PostDetail post={post}/>
                     <Author author={post.author}/>
+                    <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
                     <CommentsForm slug={post.slug}/>
                     <Comments slug={post.slug}/>
                 </div>
